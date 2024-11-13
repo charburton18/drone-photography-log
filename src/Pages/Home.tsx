0ea5239fe2d8385422e2d2de4039c876a3b1bf8a
Char B <charburton18@gmail.com>
2024-11-13 11:38:49 -0700

    commented out unused code

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
 
   const [searchQuery, setSearchQuery] = useState<any>('');
   const [answer, setAnswer] = useState<string | number>('');
-  const [dataset, setDataset] = useState<object>({});
+  // const [dataset, setDataset] = useState<object>({});
 
   const droneData = [
     {
@@ -125,19 +125,19 @@ function Home() {
   ]
 
   // this function GETs all data from the backend and saves it in the state variable called dataset.
-  const getAllData = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/');
-      const result = await response.json();
-      const allDroneData = result;
-      console.log(`Drone data received from the backend:`, allDroneData);
-      setDataset(allDroneData);
-      return;
-    }
-    catch (error: any) {
-      console.log('error', error);
-    }
-  };
+  // const getAllData = async () => {
+  //   try {
+  //     const response = await fetch('http://localhost:3000/');
+  //     const result = await response.json();
+  //     const allDroneData = result;
+  //     console.log(`Drone data received from the backend:`, allDroneData);
+  //     setDataset(allDroneData);
+  //     return;
+  //   }
+  //   catch (error: any) {
+  //     console.log('error', error);
+  //   }
+  // };
 
   // this function POSTs the searchQuery to the backend and saves the response in the state variable called answer.
   const postQueryForAnswer = async () => {
